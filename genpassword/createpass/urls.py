43e6f7df6d78279ed7683f04fd045336687219f4
Alex <lekha2000a@mail.ru>
2022-11-10 23:29:57 +0300

    Частичная поддержка русского языка

--- genpassword/createpass/urls.py
+++ genpassword/createpass/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('about/', about, name='about_site'),
     path('generate/', gen_pas, name='gen'),
     path('show_pas/', show_pas, name='show_pas'),
-    path('cleardb', cleardb, name='cleardb')
+    path('cleardb/', cleardb, name='cleardb'),
+
+    path('ru/index/', index_ru, name='index_ru'),
+    path('ru/about/', about_ru, name='about_site_ru'),
+    path('ru/generate/', gen_pas_ru, name='gen_ru'),
+    path('ru/show_pas/', show_pas_ru, name='show_pas_ru'),
 ]
--- genpassword/createpass/views.py
+++ genpassword/createpass/views.py
@@ -42,3 +42,42 @@ def pageNotFound(request, exception):
 def cleardb(request):
     # clear db
     return render(request, 'createpass/index.html')
+
+
+# Russian views
+
+def index_ru(request):
+    return render(request, 'createpass/ru/index.html')
+
+
+def show_pas_ru(request):
+    if request.GET:
+        try:
+            password_length = int(request.GET['password_length_name'])
+        except:
+            return redirect('gen', permanent=True)
+        special = request.GET.get('isSpecial_name', False)
+        # It is add layer to find out value of special
+        special = True if special == 'on' else False
+        simple_pas = request.GET.get('isMemorable', False)
+        simple_pas = True if simple_pas == 'on' else False
+        pases = [gen_password(length=password_length, special_symbols=special,
+                              simple_pas=simple_pas) for _ in range(5)]
+        data = {'completed_pass': pases, 'isSimple': False}
+        if simple_pas:
+            data['isSimple'] = True
+    else:
+        return redirect('index_ru')
+    return render(request, 'createpass/ru/show_pas.html', context=data)
+
+
+def gen_pas_ru(request):
+    return render(request, 'createpass/ru/generate_pas.html')
+
+
+def about_ru(request):
+    return render(request, 'createpass/ru/about.html')
+
+
+def pageNotFound_ru(request, exception):
+    return render(request, 'createpass/ru/pageNotFound.html')
